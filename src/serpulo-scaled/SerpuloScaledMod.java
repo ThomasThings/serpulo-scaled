416985103d5d127c0dd6292b5d809c0295206490
ThomasThings <89316672+ThomasThings@users.noreply.github.com>
2023-01-26 15:18:06 -0500

    mod.java fix

---
+++ src/serpulo-scaled/SerpuloScaledMod.java
@@ -0,0 +1,13 @@
+package serpulo-scaled;
+
+import mindustry.mod.*;
+import serpulo-scaled.content.*;
+
+public class SerpuloScaledMod extends Mod{
+
+    @Override
+
+    public void loadContent(){
+    }
+
+}
